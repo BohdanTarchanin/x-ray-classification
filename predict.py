fef3d044b6ebb4a8181257811fa0895a11de3aa5
BohdanTarchanin <32395057+BohdanTarchanin@users.noreply.github.com>
2023-10-26 21:04:38 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,62 +1,61 @@
-#importing libraries
-import streamlit as st
-import numpy as np
-import tensorflow as tf
-from PIL import Image
+#import required modules
+import streamlit as app_interface
+import numpy as np_matrixlib
+import tensorflow as tf_framework
+from PIL import Imaging
 
-# Loading the tflite model
-model_path = "main.tflite"
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
+# Initialize the tflite model
+model_directory = "primary.tflite"
+model_interpreter = tf_framework.lite.Interpreter(model_path=model_directory)
+model_interpreter.allocate_tensors()
 
-# Get input and output details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-input_shape = input_details[0]['shape']
-output_shape = output_details[0]['shape']
-input_dtype = input_details[0]['dtype']
-output_dtype = output_details[0]['dtype']
+# Retrieve input-output information
+input_info = model_interpreter.get_input_details()
+output_info = model_interpreter.get_output_details()
+input_dimensions = input_info[0]['shape']
+output_dimensions = output_info[0]['shape']
+input_type = input_info[0]['dtype']
+output_type = output_info[0]['dtype']
 
-# Define class names
-class_names = ['Covid', 'Viral Pneumonia', 'Normal']
+# Specify labels
+labels = ['Covid', 'Viral Pneumonia', 'Normal']
 
-st.set_page_config(page_title="Chest X-Ray Diagnostic Tool", layout="wide")
-col1, col2 = st.columns([1, 1])  # Divide the page into two columns
+app_interface.set_page_config(page_title="Chest X-Ray Diagnostic Tool", layout="wide")
+section1, section2 = app_interface.columns([1, 1])  # Arrange the interface in two parts
 
-# Left column - Image upload
-with col1:
-    st.title('Chest X-Ray Diagnostic Tool')
-    st.markdown('<h3 style="font-weight:normal;">This application helps to classify Chest X-ray images as either COVID-19, Viral Pneumonia or Normal. Ensure your image is clear for best results.</h3>', unsafe_allow_html=True)
+# Section for uploading images
+with section1:
+    app_interface.title('Chest X-Ray Diagnostic Tool')
+    app_interface.markdown('<h3 style="font-weight:normal;">Utilize this tool to classify Chest X-ray images into categories: COVID-19, Viral Pneumonia, or Normal. Clear images yield better outcomes.</h3>', unsafe_allow_html=True)
 
-    # Image upload
-    uploaded_file = st.file_uploader("Upload a chest X-ray image", type=["jpg", "jpeg", "png"])
+    # Upload mechanism
+    file_to_upload = app_interface.file_uploader("Choose a chest X-ray image", type=["jpg", "jpeg", "png"])
 
-    # Image prediction
-    if uploaded_file is not None:
-        # Load and preprocess the image
-        image = Image.open(uploaded_file)
-        if image.mode != "RGB":
-            image = image.convert("RGB")  # Convert to RGB if not already
-        image = image.resize((input_shape[1], input_shape[2]))
-        image = np.array(image, dtype=np.float32)
-        image /= 255.0
-        image = np.expand_dims(image, axis=0)
+    # Image analysis
+    if file_to_upload:
+        # Adjust and preprocess image
+        loaded_image = Imaging.open(file_to_upload)
+        if loaded_image.mode != "RGB":
+            loaded_image = loaded_image.convert("RGB")  # Adjust to RGB mode if different
+        loaded_image = loaded_image.resize((input_dimensions[1], input_dimensions[2]))
+        processed_image = np_matrixlib.array(loaded_image, dtype=np_matrixlib.float32)
+        processed_image /= 255.0
+        processed_image = np_matrixlib.expand_dims(processed_image, axis=0)
 
-        # Predict function
-        def predict(image):
-            interpreter.set_tensor(input_details[0]['index'], image.astype(input_dtype))
-            interpreter.invoke()
-            predictions = interpreter.get_tensor(output_details[0]['index'])
-            predicted_class_index = np.argmax(predictions, axis=1)
-            predicted_class_name = class_names[predicted_class_index[0]]
-            return predicted_class_name
+        # Function to categorize image
+        def categorize(input_img):
+            model_interpreter.set_tensor(input_info[0]['index'], input_img.astype(input_type))
+            model_interpreter.invoke()
+            results = model_interpreter.get_tensor(output_info[0]['index'])
+            category_index = np_matrixlib.argmax(results, axis=1)
+            category_name = labels[category_index[0]]
+            return category_name
         
-        # Instant prediction
-        predicted_class_name = predict(image)
-        # Display prediction as a heading in bold font
-        st.markdown(f"<h3>Classified as: <span style='font-style: italic; font-weight: bold;'>{predicted_class_name}</span></h3>", unsafe_allow_html=True)
+        # Display results instantly
+        category_result = categorize(processed_image)
+        app_interface.markdown(f"<h3>Result: <span style='font-style: italic; font-weight: bold;'>{category_result}</span></h3>", unsafe_allow_html=True)
 
-# Display uploaded image
-with col2:
-    if uploaded_file is not None:
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+# Section to show uploaded image
+with section2:
+    if file_to_upload:
+        app_interface.image(loaded_image, caption="Provided Image", use_column_width=True)
